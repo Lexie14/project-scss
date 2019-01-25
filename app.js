dd375f3c81650e714d98365740e55cebd1ae580a
Lexie <oleksandrapas@gmail.com>
2019-01-25 16:46:27 +0100

    eventListeners moved to js file

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ openMenu = () => {
 closeMenu = () => {
   document.getElementsByClassName("side-menu")[0].style.width = "0";
 };
+
+window.onload = function() {
+  document
+    .getElementsByClassName("menu-bar")[0]
+    .addEventListener("click", openMenu);
+
+  document
+    .getElementsByClassName("closeBtn")[0]
+    .addEventListener("click", closeMenu);
+};
